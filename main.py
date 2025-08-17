5deab2907354d1b6d634ddaceaf88c6aa121d664
utkarshg1 <gaikwadujg@gmail.com>
2025-08-17 12:07:30 +0530

    main.py done

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+def simple_interest(p: float, n: float, r: float) -> tuple[float, float]:
+    """
+    This function calulates simple interest and amount
+    """
+    i = (p * n * r) / 100
+    a = p + i
+    return i, a
+
 def main():
-    print("Hello from adv-python-1130!")
+    p = float(input("Principal in INR : "))
+    n = float(input("Number of Years : "))
+    r = float(input("Rate of interest in %p.a. : "))
+    i, a = simple_interest(p, n, r)
+    print(f"Simple Interest : {i:.2f} INR")
+    print(f"Amount : {a:.2f} INR")
 
 
 if __name__ == "__main__":
